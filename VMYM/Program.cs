aaf7ad48e23671281a8e6c308f47ed5c7bb04228
semvs <semvs@ayvansaray.edu.tr>
2019-03-04 09:12:17 +0300

    Add project files.

---
+++ VMYM/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varmısınyokmusun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            islem oyun = new islem();
+            // menü oluşturalım
+            Console.WriteLine(" _________   _________   _________   _________   _________");
+            Console.WriteLine(" |   1   |   |   2   |   |   3   |   |   4   |   |   5   |");
+            Console.WriteLine(" _________   _________   _________   _________   _________");
+            Console.WriteLine(" |   6   |   |   7   |   |   8   |   |   9   |   |  10   |");
+            Console.WriteLine(" _________   _________   _________   _________   _________");
+            Console.WriteLine(" |  11   |   |  12   |   |  13   |   |  14   |   |  15   |");
+            Console.WriteLine(" _________   _________   _________   _________   _________");
+            Console.WriteLine(" |  16   |   |  17   |   |  18   |   |  19   |   |  20   |");
+            Console.WriteLine(" _________   _________   _________   _________   ");
+            Console.WriteLine(" |  21   |   |  22   |   |  23   |   |  24   |   ");
+            // kutu seçimi
+            Console.WriteLine("                                                          ");
+            Console.WriteLine("            KENDİ KUTUNUZU SEÇİNİZ: (Kendi Kutunuzu ancak son seçimde açabilirsiniz.) ");                     
+            int a = Convert.ToInt32(Console.ReadLine());
+            oyun.kutu_secimi(a - 1);
+            Console.Clear();
+            Console.WriteLine("                      __________                           ");
+            Console.WriteLine("                      |   {0}   |                          ",a);
+            Console.WriteLine("                    SEÇTİĞİNİZ KUTU                        ");
+            Console.ReadLine();
+            Console.Clear();
+            // kutu seçimi
+            for (int i = 0; i < 7; i++)
+            {
+                if (i == 0)
+                {
+                    for (int z = 0; z < 8; z++)
+                    {
+                        int x = 8;
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   1   |   |   2   |   |   3   |   |   4   |   |   5   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   6   |   |   7   |   |   8   |   |   9   |   |  10   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  11   |   |  12   |   |  13   |   |  14   |   |  15   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  16   |   |  17   |   |  18   |   |  19   |   |  20   |");
+            Console.WriteLine(" _________   _________   _________   _________   ");
+            Console.WriteLine(" |  21   |   |  22   |   |  23   |   |  24   |   ");
+                        Console.WriteLine("");
+                        Console.WriteLine("Kendi Kutunuzu ancak son seçimde açabilirsiniz.");
+                        Console.WriteLine("Daha önce açtığınız kutular:");
+                        for (int g = 0; g <24; g++)
+                        {
+                            oyun.acılan_kutular(g);
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("              {0} Kutu açmamız gerekiyor.       ", x);
+                        x--;
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        //kutu açma 
+                        int c = oyun.kutu_acma(b - 1);
+                        oyun.acılan_kutuları_sıfırlama(b - 1);
+                        Console.Clear();
+                        Console.WriteLine("                      __________                          ");
+                        Console.WriteLine("                      |   {0}   |                         ", c);
+                        Console.WriteLine("                      AÇILAN KUTU                         ");
+                        Console.WriteLine("Enter tuşuna basınız.");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                }
+                    else if (i==1)
+                    {
+                    for (int k = 0; k < 7;k++)
+                    {
+                        int y = 7;
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   1   |   |   2   |   |   3   |   |   4   |   |   5   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   6   |   |   7   |   |   8   |   |   9   |   |  10   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  11   |   |  12   |   |  13   |   |  14   |   |  15   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                         Console.WriteLine(" |  16   |   |  17   |   |  18   |   |  19   |   |  20   |");
+            Console.WriteLine(" _________   _________   _________   _________   ");
+            Console.WriteLine(" |  21   |   |  22   |   |  23   |   |  24   |   ");
+                        Console.WriteLine("");
+                        Console.WriteLine("Daha önce açtığınız kutular:");
+                        for (int g = 0; g < 24; g++)
+                        {
+                            
+                            oyun.acılan_kutular(g);
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("              {0} Kutu açmamız gerekiyor.        ", y);
+                        y--;
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        //kutu açma 
+                        int c = oyun.kutu_acma(b - 1);
+                        oyun.acılan_kutuları_sıfırlama(b - 1);
+                        Console.Clear();
+                        Console.WriteLine("                      __________                          ");
+                        Console.WriteLine("                      |   {0}   |                         ", c);
+                        Console.WriteLine("                      AÇILAN KUTU                         ");
+                        Console.WriteLine("Enter tuşuna basınız.");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    }
+                 else if(i==2)
+                    {
+                    for (int k = 0; k < 6;k++)
+                    {
+                        int p = 6;
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   1   |   |   2   |   |   3   |   |   4   |   |   5   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   6   |   |   7   |   |   8   |   |   9   |   |  10   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  11   |   |  12   |   |  13   |   |  14   |   |  15   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  16   |   |  17   |   |  18   |   |  19   |   |  20   |");
+            Console.WriteLine(" _________   _________   _________   _________   ");
+            Console.WriteLine(" |  21   |   |  22   |   |  23   |   |  24   |   ");
+                        Console.WriteLine("");
+                        Console.WriteLine("Daha önce açtığınız kutular:");
+                        for (int g = 0; g < 24; g++)
+                        {
+                            
+                            oyun.acılan_kutular(g);
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("              {0} Kutu açmamız gerekiyor        ", p);
+                        p--;
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        //kutu açma 
+                        int c = oyun.kutu_acma(b - 1);
+                        oyun.acılan_kutuları_sıfırlama(b - 1);
+                        Console.Clear();
+                        Console.WriteLine("                      __________                          ");
+                        Console.WriteLine("                      |   {0}   |                         ", c);
+                        Console.WriteLine("                      AÇILAN KUTU                         ");
+                        Console.WriteLine("Enter tuşuna basınız.");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    }
+                else if(i==3)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        int r = 3;
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   1   |   |   2   |   |   3   |   |   4   |   |   5   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |   6   |   |   7   |   |   8   |   |   9   |   |  10   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  11   |   |  12   |   |  13   |   |  14   |   |  15   |");
+                        Console.WriteLine(" _________   _________   _________   _________   _________");
+                        Console.WriteLine(" |  16   |   |  17   |   |  18   |   |  19   |   |  20   |");
+            Console.WriteLine(" _________   _________   _________   _________   ");
+            Console.WriteLine(" |  21   |   |  22   |   |  23   |   |  24   |   ");
+                        Console.WriteLine("");
+                        Console.WriteLine("Daha önce açtığınız kutular:");
+                        for (int g = 0; g < 248; g++)
+                        {
+
+                            oyun.acılan_kutular(g);
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("              {0} Kutu açmamız gerekiyor        ", r);
+                        r--;
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        //kutu açma 
+                        int c = oyun.kutu_acma(b - 1);
+                        oyun.acılan_kutuları_sıfırlama(b - 1);
+                        Console.Clear();
+                        Console.WriteLine("                      __________                          ");
+                        Console.WriteLine("                      |   {0}   |                         ", c);
+                        Console.WriteLine("                      AÇILAN KUTU                         ");
+                        Console.WriteLine("Enter tuşuna basınız.");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                }
+                
+
+
+
+                //teklif verme 
+                Console.WriteLine("               HAMDİ BEY ARIYOR...");
+                Console.WriteLine("             -HAMDİ BEY'İN TEKLİFİ-");
+                int d = oyun.teklif_ver();
+                Console.WriteLine("                   {0}         TL", d);
+                Console.WriteLine("Hamdi Bey'in {0}'tl teklifine var mısın yok musun?", d);
+                Console.WriteLine("(Varım:1  Yokum:-1)");
+                string vy = Console.ReadLine();
+                if (vy == "1")
+                {
+                    Console.WriteLine("Varım diyor!!! ve {0} tl kazanıyor.", d);
+                    break;
+                }
+                else if (vy =="-1")
+                {
+                    Console.WriteLine("Yokum diyor!!! Yarşmaya devam ediyor.");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+            Console.WriteLine("Başka Seçim Hakkınız Kalmadı.");
+            Console.WriteLine("Oyun Bitti.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VMYM/islem.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varmısınyokmusun
+{
+    class islem
+    {
+        private int[] kutular=new int[24]{1,2,5,10,25,50,75,100,200,300,400,500,750,5000,10000,15000,25000,50000,75000,100000,150000,250000,500000,500000};
+        private int[] rastgele_kutular = new int[24];
+        public int indis;
+        private int secilen_kutu;
+        public islem()
+        {
+            //rastegele indis üretme
+            for (int i = 0; i <kutular.Length; i++)
+            {
+                int[] indistutucu = new int[24];
+                Random rnd = new Random();
+                indis = rnd.Next(0,24);
+                int kutu = kutular[indis];
+                if (kutu == 0) 
+                { 
+                    i--; 
+                    continue; 
+                }
+                if (kutu != 0) 
+                { 
+                    rastgele_kutular[i]=kutu ;
+                    kutular[indis] = 0;
+                }
+             }
+
+        }
+        public int[] kutu_goster() 
+        {
+            return rastgele_kutular;
+        }
+        public int kutu_secimi(int secilen_kutu_indisi)
+        {
+            int alim = 0;
+            if (rastgele_kutular[secilen_kutu_indisi] != 0)
+            {
+                secilen_kutu = rastgele_kutular[secilen_kutu_indisi];
+                return rastgele_kutular[secilen_kutu_indisi];
+            }
+            else
+            { 
+                for(int i = 0; i<rastgele_kutular.Length;i++)
+                {
+                    if (rastgele_kutular[i] != 0)
+                    {
+                        alim = i;
+                        break;
+                    }
+                }
+                secilen_kutu = rastgele_kutular[alim];
+                return rastgele_kutular[alim];
+            }
+        }
+        public int kutu_acma(int acılan_kutu_indisi)
+        {
+            return rastgele_kutular[acılan_kutu_indisi];
+        }
+        public void acılan_kutuları_sıfırlama(int acılan_kutu_indisi)
+        {
+            rastgele_kutular[acılan_kutu_indisi] = 0;
+        }
+        public int teklif_ver()
+        {
+            int kutu_deger_toplamı = 0;
+            for (int i = 0; i < rastgele_kutular.Length; i++)
+            {
+                kutu_deger_toplamı += rastgele_kutular[i];
+            }
+            int teklif =( kutu_deger_toplamı / (rastgele_kutular.Length + 20));
+            return teklif;
+        }
+        public void acılan_kutular(int p)
+        {
+
+            if (rastgele_kutular[p] == 0)
+            {
+                Console.Write("-");
+                Console.Write(p+1);
+                Console.Write("-");
+            }
+            
+        }  
+    }
+}
